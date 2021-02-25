d618b46c432b8cdcb1b4e0b75713c97bd0f46f36
Jayesh Mann <jayeshmann06@gmail.com>
2021-02-25 14:19:08 +0530

    Server & Client networking updated Server keeps running, So that client can be re-run several times.

--- client.java
+++ client.java
@@ -1,23 +1,25 @@
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class client {
   public static void main(String[] args) throws IOException {
     Socket cs;
-
-
     cs = new Socket("127.0.0.1", 900);
     InputStream in = cs.getInputStream();
-    BufferedReader br = new BufferedReader(new InputStreamReader(in));
-    String s = br.readLine();
+    Scanner br = new Scanner(new InputStreamReader(in));
+    String s = br.nextLine();
     System.out.println(s);
-    BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
-    String st = buf.readLine();
-    System.out.println(st);
+    Scanner sc = new Scanner(System.in);
+    String name = sc.nextLine();
 
+    PrintStream pStream = new PrintStream(cs.getOutputStream());
+    pStream.println(name);
+    System.out.println(br.nextLine());
     cs.close();
+    sc.close();
   }
 }
--- server.java
+++ server.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -6,14 +8,18 @@ import java.net.Socket;
 public class server {
 
   public static void main(String[] args) throws IOException {
-    int port = 900;
-    Socket s;
-    ServerSocket ss = new ServerSocket(port);
-    s = ss.accept();
-    System.out.println("Server running on port: " + port);
-    PrintStream ps = new PrintStream(s.getOutputStream());
-    ps.println("Enter name: ");
-
-    ss.close();
+    while (true) {
+      int port = 900;
+      Socket s;
+      ServerSocket ss = new ServerSocket(port);
+      s = ss.accept();
+      System.out.println("Server running on port: " + port);
+      PrintStream ps = new PrintStream(s.getOutputStream());
+      ps.println("Enter name: ");
+      BufferedReader buf = new BufferedReader(new InputStreamReader(s.getInputStream()));
+      String name = buf.readLine();
+      ps.println("Hello " + name + "!");
+      ss.close();
+    }
   }
 }
