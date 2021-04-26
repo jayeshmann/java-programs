ecb070c592d29cdb8c6b4ce062f974bfd7b1f57f
kirti bansal <kirtibansal190@gmail.com>
2021-04-26 20:45:46 +0530

    better hello world

--- QuickStart.java
+++ QuickStart.java
@@ -3,6 +3,6 @@ class QuickStart {
 
     char[] c = {'a', 'b', 'c'};
     String s = new String(c, 1, 2);
-    System.out.println("Hello, World. " + s);
+    System.out.println("Hello, World. ");
   }
 }
